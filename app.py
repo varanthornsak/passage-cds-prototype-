0587cf3efdb885c344518b6296134678eb28fc9a
varanthornsak <varanthorn.s.ysda@gmail.com>
2026-02-20 22:07:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import streamlit as st
 
+st.title("PASSAGE-CDS Prototype")
+
+age = st.number_input("Age", 40, 100)
+adl = st.slider("ADL Score", 0, 6)
+
+risk = 0
+if age > 75:
+    risk += 2
+if adl < 4:
+    risk += 2
+
+if risk <= 2:
+    st.success("Low Risk")
+else:
+    st.error("High Risk")
