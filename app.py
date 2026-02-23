20472805f3d985cb931aa11d27384535f482bf07
varanthornsak <varanthorn.s.ysda@gmail.com>
2026-02-23 22:59:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -765,12 +765,13 @@ st.metric("Estimated Annual Revenue (THB)", f"{revenue:,.0f}")
 # PLATFORM FOOTER
 # ==========================================================
 
-st.markdown(""")
+st.markdown("""
 <div class='footer'>
 PASSAGE AI Platform | Faculty of Medicine, Khon Kaen University<br>
 Digital Preventive Oncology & AI Research Initiative<br>
 Prototype SaaS Demonstration Version
 </div>
+""", unsafe_allow_html=True)
 # ==========================================================
 # ===== PASSAGE PROFESSIONAL ML EXTENSION (DROP-IN) ======
 # ==========================================================
